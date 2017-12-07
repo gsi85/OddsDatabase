d7438a14805e5a3aa598301accf8bc13f83036d8
Laszlo Sisa <v-lsisa@expedia.com>
2017-12-07 16:34:40 +0100

    find by cards in hand

--- src/main/java/oddsdatabase/repository/OddsRepository.java
+++ src/main/java/oddsdatabase/repository/OddsRepository.java
@@ -2,9 +2,12 @@ package oddsdatabase.repository;
 
 import oddsdatabase.model.OddsEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface OddsRepository extends CrudRepository<OddsEntity, Long> {
+
+    OddsEntity findByCardsInHand(@Param("cardsInHand") String cardsInHand);
 
 }
